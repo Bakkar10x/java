08dd4b4bb70b136bbe5658e4f9e385c0da8b2d62
Abu <bakkar10x@gmail.com>
2023-02-14 22:07:16 -0800

    initial commit

---
+++ 2nd-class/2nd-class.js
@@ -0,0 +1,28 @@
+// js data type
+// string data type
+var name = ('Amirul');
+
+// number data type
+var price = (20, 30, 40);
+
+// boolean data type
+var nameAmrul = true;
+var nameFiroz = false;
+
+
+// js comment 2 types
+
+// single line comment
+/* multiple line comment */
+
+// js keyword
+/*var ;
+function;
+const;
+true;
+false;
+if;
+else;
+for;
+while;
+etc */
